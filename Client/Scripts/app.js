677f632b2396485191214a7cc60918c74102161b
Jerome Ching <nathan_9_9@hotmail.com>
2021-06-02 15:55:31 -0400

    Heroku Init

--- Client/Scripts/app.js
+++ Client/Scripts/app.js
@@ -1,8 +1,16 @@
 "use strict";
 (function () {
+    let btn1 = document.getElementById("clickMe");
+    let counter = 0;
     function Start() {
         console.log("app Started");
     }
+    function ClickMe() {
+        let firstP = document.getElementById("firstP");
+        counter++;
+        firstP.innerText = `Clicked ${counter}`;
+    }
     window.addEventListener('load', Start);
+    btn1.addEventListener("click", ClickMe);
 })();
 //# sourceMappingURL=app.js.map
--- Client/Scripts/app.ts
+++ Client/Scripts/app.ts
@@ -3,12 +3,21 @@
 
 
 (function(){
+let btn1 = document.getElementById("clickMe");
+let counter = 0;
 
     function Start():void
     {
         console.log("app Started");
-
+    }
+    function ClickMe()
+    {
         
+        let firstP = document.getElementById("firstP");
+        counter++;
+        firstP.innerText = `Clicked ${counter}`;
+
     }
     window.addEventListener('load', Start);
+    btn1.addEventListener("click", ClickMe);
 })();
