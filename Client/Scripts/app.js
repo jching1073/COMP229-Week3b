2d99fca4139baa5fd028f70a33ea9286f72a81b7
Jerome Ching <nathan_9_9@hotmail.com>
2021-06-02 13:57:33 -0400

    added bootstrap and font awesome support

---
+++ Client/Scripts/app.js
@@ -0,0 +1,8 @@
+"use strict";
+(function () {
+    function Start() {
+        console.log("app Started");
+    }
+    window.addEventListener('load', Start);
+})();
+//# sourceMappingURL=app.js.map
---
+++ Client/Scripts/app.ts
@@ -0,0 +1,13 @@
+//IIFE
+"use strict";
+
+
+(function(){
+
+    function Start():void
+    {
+        console.log("app Started");
+
+        window.addEventListener('load', Start);
+    }
+})();
